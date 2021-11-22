6d201d6aae1cdca7edcd12de9311c8a04d254bb6
Adam Peters <petersadam18@gmail.com>
2021-11-21 19:58:13 -0500

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component style={{ backgroundColor: 'blue' }} {...pageProps} />
 }
 
 export default MyApp
